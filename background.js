c7dc40f62bc7952322a7a28660503eaf8b812a1c
John Sylvain <jsylvain007@gmail.com>
2017-05-03 18:29:46 -0400

    init commit

---
+++ background.js
@@ -0,0 +1,30 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    title: 'I\'m Feeling Lucky',
+    contexts:['selection'],
+    id: 'im-feeling-lucky'
+  })
+});
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  var url = "http://www.google.com/search?q=" + encodeURI(text) + "&btnI=Im+Feeling+Lucky";
+  navigate(url);
+});
+
+chrome.contextMenus.onClicked.addListener(function(info) {
+  var url = 'http://www.google.com/search?q=' + encodeURI(info.selectionText) +'&btnI=Im+Feeling+Lucky';
+  navigate(url);
+})
+
+function navigate(url, opts) {
+  if(opts.newtab) {
+    chrome.tabs.create({
+      url: url,
+      active: false
+    })
+  } else if (!opts.newtab) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.update(tabs[0].id, {url: url});
+    });
+  }
+}
